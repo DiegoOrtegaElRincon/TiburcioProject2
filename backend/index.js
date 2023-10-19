0fe596238f626b6744b7cae9acd127a3d69f13c9
DiegoOrtegaElRincon <diegoalejandroortegamanzano@alumno.ieselrincon.es>
2023-10-19 18:24:00 +0200

    Backend fully works

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+
+var path = require('path');
+
+const app = express();
+
+// public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+var corsOptions = {
+  origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./models");
+
+// normal use. Doesn't delete the database data
+// db.sequelize.sync().then(() => {
+//   console.log("Synced db.");
+// }).catch((err) => {
+//   console.log("Failed to sync db: " + err.message);
+// });
+
+
+// In development, you may need to drop existing tables and re-sync database
+// Use this just for development
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bicycle application." });
+});
+
+require("./routes/bicycle.routes")(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
